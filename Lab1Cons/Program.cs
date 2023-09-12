b6884ddcbe081bb50e97e1596041cd705cd64e1a
PIBO0120014 Ащеулов Андрей Витальевич <PIBO0120014@sibupk.su>
2023-09-12 19:25:33 +0700

    v4

--- Lab1Cons/Program.cs
+++ Lab1Cons/Program.cs
@@ -4,8 +4,8 @@ namespace Lab1Cons
 {
     /*
     Задание
-    h-кол яч в масиве, заполняются от 1 до 100 рандомом
-    находиться H-сумма ячеек, повторяется процес несколько раз и находится среднее
+    h - кол яч в масиве (16,32,64...) , заполняются от 1 до 100 рандомом
+    находиться H-сумма ячеек, повторяется процес несколько раз (M) и находится среднее
      
      */
     class Program
@@ -15,10 +15,27 @@ namespace Lab1Cons
             Console.WriteLine("Размер словаря:");
             int h = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Объем выберки :");
-            int v = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Квантиль:");
             int q = Convert.ToInt32(Console.ReadLine());
-            
+
+            int[] dir = new int[h];
+            Random r = new Random();
+            int argvol = 0;
+
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < dir.Length; i++)
+                {
+                    dir[i] = r.Next(1, 100);
+                    argvol += dir[i];
+                }
+            }
+           
+            //for (int i = 0; i < dir.Length; i++)
+            //{
+            //    Console.WriteLine(dir[i]);
+            //}
         }
        
     }
