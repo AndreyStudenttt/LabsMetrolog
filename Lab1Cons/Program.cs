afa0942fc22b23f8f32de5f09a74f7e7421cd709
PIBO0120014 Ащеулов Андрей Витальевич <PIBO0120014@sibupk.su>
2023-09-12 18:49:58 +0700

    мв

---
+++ Lab1Cons/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab1Cons
+{
+    /*
+    Задание
+    
+     
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Размер словаря:");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Объем выберки :");
+            int v = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Квантиль:");
+            int q = Convert.ToInt32(Console.ReadLine());
+            
+        }
+       
+    }
+}
